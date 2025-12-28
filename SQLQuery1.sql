68b85b92f3d08ad34a348a82e678747882b93036
TANUJA DESHMUKH <137862508+TANUJA909@users.noreply.github.com>
2025-12-28 14:00:43 +0530

    Initial Commit

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,9 +1,7 @@
 USE InterviewDB;
-GO
 
 IF OBJECT_ID('dbo.Users', 'U') IS NOT NULL 
   DROP TABLE dbo.Users;
-GO
 
 CREATE TABLE dbo.Users (
     id INT PRIMARY KEY IDENTITY(1,1),
@@ -11,4 +9,3 @@ CREATE TABLE dbo.Users (
     email VARCHAR(100) NOT NULL UNIQUE,
     created_at DATETIME DEFAULT GETDATE()
 );
-GO
